86e4ef94a75515e252572cb714893fc8b085f6d0
jojo <281982108@qq.com>
2020-11-17 15:26:48 +0800

    Initial commit

---
+++ xirr/src/main/java/com/jojo/xirr/XIRR.java
@@ -0,0 +1,15 @@
+package com.jojo.xirr;
+
+/**
+ * ********************
+ * project name：XIRR
+ *
+ * @Author jojo
+ * email:281982108@qq.com
+ * create time 2020/11/17 15:24
+ * purpose：
+ * ********************
+ */
+public class XIRR {
+    
+}
